d74fe3214e5ce800f7d5fe96e193ec50556cd688
Francisco <83992993+franciscojben@users.noreply.github.com>
2023-07-26 15:02:40 -0600

    Add dir and file checking

--- processor/compiler.py
+++ processor/compiler.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import post_input.load_input as load_input
 import post_processor.processor as processor
 import post_output.create_output as output
@@ -7,12 +8,54 @@ logging.basicConfig(filename='./logs/processor.log', level=logging.DEBUG, filemo
 
 def parse_args():
     """
-    Parse the script arguments
+    Parse the script arguments.
+    Currently there are no script arguments.
     """
+    pass
+
+
+def check_dirs():
+    """
+    Check the directory structure to see if the required
+    directories exist.  If they do not, create the directories
+    and exit the program.
+    """
+    REQUIRED = ('data_domain', 'data_twitter', 'output', 'saved', 'logs')
+    MISSING = []
+    for directory in REQUIRED:
+        if not os.path.isdir(f'./{directory}'):
+            MISSING.append(directory)
+    if MISSING:
+        print('[MediaCAT] Directories not found; creating directories...')
+        for directory in MISSING:
+            os.mkdir(f'./{directory}')
+        print('[MediaCAT] Directories created; please add data')
+        exit()
+
+
+def check_files():
+    """
+    Check the directory structure to see if the required
+    files exist.  If they do not, exit the program.
+    """
+    REQUIRED = ('crawl_scope.csv', 'citation_scope.csv')
+    MISSING = []
+    for file in REQUIRED:
+        if not os.path.isfile(f'./{file}'):
+            MISSING.append(file)
+    if MISSING:
+        print('[MediaCAT] Files not found; please add files')
+        exit()
 
 
 if __name__ == '__main__':
+    ## SCRIPT PREPARATION ##
+    # parse script arguments
     parse_args()
+
+    # check directories and files
+    check_dirs()
+    check_files()
 
     ## LOAD INPUT DATA ##
     # load scopes
