30f6aaa2fc21dc56c3765ba654782bfd11252cee
Francisco <83992993+franciscojben@users.noreply.github.com>
2023-08-10 21:56:00 -0400

    Fix processor meta issue

--- processor/post_processor/processor.py
+++ processor/post_processor/processor.py
@@ -1,29 +1,27 @@
+import ast
+import logging
+import pandas as pd
+import dask.dataframe as dd
 from post_processor.processor_domain import process_domain
 from post_processor.processor_twitter import process_twitter
 from timeit import default_timer as timer
-import pandas as pd
-import ast
-import dask.dataframe as dd
-import logging
 
 
 def init():
-    """
-    Initialize processor script.
-    """
+    '''Initialize processor script.'''
     logging.basicConfig(filename='./logs/processor.log',
                         level=logging.DEBUG, filemode='w')
 
 
 def parse_referrals(article, domain_referrals, twitter_referrals):
-    """
+    '''
     Cross-match the referrals for domain referrals and twitter referrals.
     Returns a list of all sources that are referring to this specific article.
     Parameters:
       article: an article in the data
       domain_referrals: a dictionary of all the referrals in the domain data
       twitter_referrals: a dictionary of all the referrals in the twitter data
-    """
+    '''
     referring_articles = []
     # get all referrals for this url (who is referring to me)
 
@@ -44,17 +42,17 @@ def parse_referrals(article, domain_referrals, twitter_referrals):
 
 
 def get_num_ref(row):
-    """
+    '''
     Return the number of referrals.
-    """
+    '''
     try:
-        return len(ast.literal_eval(row["referring name"]))
+        return len(ast.literal_eval(row['referring name']))
     except Exception:
         return 0
 
 
 def process_crawler(crawl_scope, citation_scope):
-    """
+    '''
     The main point of entry for the processor.
     Calls the domain and twitter processor seperately.
     Parameters:
@@ -69,54 +67,38 @@ def process_crawler(crawl_scope, citation_scope):
                                 with mapping of url to id.
 
     Outputs the post processing data into output.json and interest_output.json.
-    """
+    '''
     # initialize script
     init()
 
-    # crawling
+    # process crawling results
     domain_referrals, domain_data = process_domain(crawl_scope, citation_scope)
     twitter_referrals, twitter_data = process_twitter(
         crawl_scope, citation_scope)
+    logging.info(f'domain columns: {domain_data.columns}')
+    logging.info(f'twitter columns: {twitter_data.columns}')
 
-    logging.info(domain_data.columns)
-    ### Add referrals to domain_data ###
+    ### Add referrals to domain_data ### 
     domain_data['url_dup'] = domain_data.index
     domain_data['referring name'] = domain_data.apply(parse_referrals, axis=1, args=(
-        domain_referrals, twitter_referrals, ), meta='object')
+        domain_referrals, twitter_referrals, ), meta=('referring name', 'str'))
     domain_data['number of referrals'] = domain_data.apply(
-        get_num_ref, axis=1, meta='object')
-    # domain_data.assign(referring_name=len(
-    #     ast.literal_eval(domain_data["referring name"].str)))
-    # logging.info(domain_res.index)
-    # domain_res_pd = pd.DataFrame(domain_res, columns=[
-    #     'referring name',
-    #     'number of referrals'], index=domain_res.index)
-    # domian_data_pd = domain_data.compute()  # domian_data_pd is a panda dataframe
-    # domian_data_pd.update(domain_res_pd)
-    # dd.from_pandas(domian_data_pd, npartitions=1)
+        get_num_ref, axis=1, meta=('number of referrals', 'int64'))
     processed_domain_data = domain_data
 
-    ### Add referrals to twitter_data ###
+    ### Add referrals to twitter_data ###    
     twitter_data['referring name'] = ''
     twitter_data['number of referrals'] = ''
     twitter_data['url_dup'] = twitter_data.index
     twitter_data['referring name'] = twitter_data.apply(parse_referrals, axis=1, args=(
-        domain_referrals, twitter_referrals, ), meta='object')
+        domain_referrals, twitter_referrals, ), meta=('referring name', 'str'))
     twitter_data['number of referrals'] = twitter_data.apply(
-        get_num_ref, axis=1, meta='object')
-    # twitter_res = twitter_data.apply(parse_referrals, axis=1, args=(
-    #     domain_referrals, twitter_referrals, ), meta='object')
-    # twitter_res_pd = pd.DataFrame(twitter_res, columns=[
-    #     'referring name',
-    #     'number of referrals'], index=twitter_res.index)
-    # # twitter_data_pd is a panda dataframe
-    # twitter_data_pd = twitter_data.compute()
-    # twitter_data_pd.update(twitter_res_pd)
-    # processed_twitter_data = dd.from_pandas(twitter_data_pd, npartitions=1)
+        get_num_ref, axis=1, meta=('number of referrals', 'int64'))
     processed_twitter_data = twitter_data
 
     ### Save the processed data ###
     processed_domain_data.to_parquet(
-        './saved/processed_domain_data.parquet', engine="pyarrow")
+        './saved/processed_domain_data.parquet', engine='pyarrow')
     processed_twitter_data.to_parquet(
-        './saved/processed_twitter_data.parquet', engine="pyarrow")
+        './saved/processed_twitter_data.parquet', engine='pyarrow')
+
